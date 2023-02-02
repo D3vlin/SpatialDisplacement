f288faebab39234d90aeed640dea7240fafb98af
D3vlin <alexisdq16@gmail.com>
2023-02-02 12:55:22 -0500

    added gamestate, playercontroller

--- Source/SpatialDisplacement/SpatialDisplacement.Build.cs
+++ Source/SpatialDisplacement/SpatialDisplacement.Build.cs
@@ -12,9 +12,11 @@ public class SpatialDisplacement : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+		PrivateIncludePaths.Add("SpatialDisplacement");
+
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
